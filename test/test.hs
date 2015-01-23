0b567b8a172f08fc0a3bd47e3c88eb4977e9a929
Michael Alan Dorman <mdorman@ironicdesign.com>
2015-01-23 05:44:44 -0500

    Server support (explicit, mostly complete).

--- test/test.hs
+++ test/test.hs
@@ -2,8 +2,9 @@ module Main where
 
 import Test.Tasty
 import Couch.Internal
+import Couch.Explicit
 
 main :: IO ()
 main = do
-  let allTests = testGroup "All tests" [internalTests]
+  let allTests = testGroup "All tests" [internalTests, explicitTests]
   defaultMain allTests
