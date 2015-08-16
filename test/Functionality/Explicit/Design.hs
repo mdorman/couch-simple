54b29f027bcfbbde4d8d9dc81c7fda01c29c3552
Michael Alan Dorman <mdorman@ironicdesign.com>
2015-08-16 17:45:15 -0400

    Test for get--db-_design-ddoc.

--- test/Functionality/Explicit/Design.hs
+++ test/Functionality/Explicit/Design.hs
@@ -5,7 +5,7 @@
 module Functionality.Explicit.Design where
 
 import           Data.Maybe                     (Maybe (Just, Nothing))
-import qualified Database.Couch.Explicit.Design as Design (size)
+import qualified Database.Couch.Explicit.Design as Design (get, size)
 import           Database.Couch.Types           (Context, CouchError (..),
                                                  ctxDb, docGetDoc)
 import           Functionality.Util             (makeTests, runTests,
@@ -21,6 +21,7 @@ _main = runTests tests
 tests :: Manager -> TestTree
 tests = makeTests "Tests of the design doc interface"
           [ ddocSize
+          , ddocGet
           ]
 
 -- Doc-oriented functions
@@ -31,4 +32,11 @@ ddocSize =
     , testAgainstSchema "Get standard _auth ddoc in _users"  (\c -> Design.size docGetDoc "_auth" Nothing c { ctxDb = Just "_users" })
 
                  "head--db-_design-ddoc.json"
+    ]
+
+ddocGet :: IO Context -> TestTree
+ddocGet =
+  makeTests "Get design document content"
+    [ testAgainstSchema "Get standard _auth ddoc in _users"  (\c -> Design.get docGetDoc "_auth" Nothing c { ctxDb = Just "_users" })
+                 "get--db-_design-ddoc.json"
     ]
