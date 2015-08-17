2e98b0160274363bc5ab222308a84036fad3ed5d
Michael Alan Dorman <mdorman@ironicdesign.com>
2015-08-17 07:10:26 -0400

    Tests for put--db-_design-ddoc.

--- test/Functionality/Explicit/Design.hs
+++ test/Functionality/Explicit/Design.hs
@@ -4,13 +4,20 @@
 
 module Functionality.Explicit.Design where
 
+import           Control.Monad                  (return)
+import           Data.Aeson                     (Value, object)
+import           Data.Either                    (Either (Right))
+import           Data.Function                  (($))
 import           Data.Maybe                     (Maybe (Just, Nothing))
-import qualified Database.Couch.Explicit.Design as Design (get, size)
+import           Data.Text.IO                   (putStrLn)
+import qualified Database.Couch.Explicit.Design as Design (get, put, size)
+import           Database.Couch.Response        (getKey)
 import           Database.Couch.Types           (Context, CouchError (..),
-                                                 ctxDb, docGetDoc)
+                                                 CouchResult, DesignDoc (..),
+                                                 ctxDb, docGetDoc, docPutParam)
 import           Functionality.Util             (makeTests, runTests,
                                                  testAgainstFailure,
-                                                 testAgainstSchema)
+                                                 testAgainstSchema, withDb)
 import           Network.HTTP.Client            (Manager)
 import           System.IO                      (IO)
 import           Test.Tasty                     (TestTree)
@@ -22,6 +29,7 @@ tests :: Manager -> TestTree
 tests = makeTests "Tests of the design doc interface"
           [ ddocSize
           , ddocGet
+          , ddocPut
           ]
 
 -- Doc-oriented functions
@@ -40,3 +48,22 @@ ddocGet =
     [ testAgainstSchema "Get standard _auth ddoc in _users"  (\c -> Design.get docGetDoc "_auth" Nothing c { ctxDb = Just "_users" })
                  "get--db-_design-ddoc.json"
     ]
+
+ddocPut :: IO Context -> TestTree
+ddocPut =
+  makeTests "Create and update a design document"
+    [ withDb $ testAgainstSchema "Simple add of document" (Design.put docPutParam "foo" Nothing initialDdoc) "put--db-_design-ddoc.json"
+    , withDb $ testAgainstFailure "Failure to update document" (\c -> do
+                                                                    _ :: CouchResult Value <- Design.put docPutParam "foo" Nothing initialDdoc c
+                                                                    Design.put docPutParam "foo" Nothing initialDdoc c) Conflict
+    , withDb $ testAgainstSchema
+                 "Add, then update a doc"
+                 (\c -> do
+                    res <- Design.put docPutParam "foo" Nothing initialDdoc c
+                    let (Right (id, _)) = getKey "id" res
+                    let (Right (rev, _)) = getKey "rev" res
+                    Design.put docPutParam "foo" (Just rev) initialDdoc {ddocId = id, ddocRev = rev} c)
+                 "put--db-docid.json"
+    ]
+  where
+    initialDdoc = DesignDoc "" "" Nothing Nothing Nothing Nothing Nothing Nothing Nothing Nothing
