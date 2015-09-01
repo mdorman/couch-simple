8932e7db535c39d15f6ac0367ac1e12e53eb4f98
Michael Alan Dorman <mdorman@ironicdesign.com>
2015-09-01 06:39:33 -0400

    Don't try to test against couchdb if it's not available.
    Now we just set ourselves up to declare a dependency on couchdb, but
    that doesn't mean that it's running, and I'm not inclined at this moment
    to figure out how to spin up a temporary instance for testing---so we
    probe, and just skip our functional tests if it's not availble.

--- test/Main.hs
+++ test/Main.hs
@@ -1,19 +1,30 @@
-{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE NoImplicitPrelude   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
-import qualified Functionality (tests)
-import qualified Quality       (tests)
-import           System.IO     (IO)
-import           Test.Tasty    (TestTree, defaultMain, testGroup)
+import           Control.Monad             (liftM, return, (=<<))
+import           Control.Monad.Catch       (SomeException, handle)
+import           Data.Bool                 (Bool (False))
+import           Data.Function             (($), (.))
+import           Data.Monoid               ((<>))
+import qualified Functionality             (tests)
+import           Network.HTTP.Client       (defaultManagerSettings, httpNoBody,
+                                            newManager, parseUrl,
+                                            responseStatus)
+import           Network.HTTP.Types.Status (statusIsSuccessful)
+import qualified Quality                   (tests)
+import           System.IO                 (IO)
+import           Test.Tasty                (TestTree, defaultMain, testGroup)
 
 -- Run all teests by default
 main :: IO ()
-main = defaultMain tests
+main = defaultMain . tests =<< do
+  manager <- newManager defaultManagerSettings
+  request <- parseUrl "http://localhost:5984/"
+  handle (\(_ :: SomeException) -> return False) $ liftM (statusIsSuccessful . responseStatus) (httpNoBody request manager)
 
 -- Everything we can throw at it
-tests :: TestTree
-tests = testGroup "All tests" [
-  Functionality.tests,
-  Quality.tests
-  ]
+tests :: Bool -> TestTree
+tests haveCouch = testGroup "All tests" $
+  [Quality.tests] <> [Functionality.tests | haveCouch]
